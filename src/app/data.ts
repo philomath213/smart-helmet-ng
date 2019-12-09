ea5bfececdfcf0dbd8fc7125f0f5d98ba0db4454
philomath213 <bilalphilomath@gmail.com>
2019-12-09 18:09:51 +0100

    Data Model

---
+++ src/app/data.ts
@@ -0,0 +1,9 @@
+export class Data {
+    helmet_id: number;
+    datetime: Date;
+    is_removed: Boolean;
+    collision: number;
+    // air_quality: number;
+    temperature: number;
+    humidity: number;
+}
